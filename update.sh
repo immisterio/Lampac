312f7551bd280d33639e6053e76c0fc999b65427
immisterio <96652250+immisterio@users.noreply.github.com>
2022-12-02 09:12:41 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ systemctl stop lampac
 
 cd $DEST
 wget https://github.com/immisterio/Lampac/releases/latest/download/publish.zip
-unzip publish.zip
+unzip -o publish.zip
 rm -f publish.zip
 
 systemctl start lampac
