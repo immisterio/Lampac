fd155d7e4fb85e2de36443195c15293085f39b4c
immisterio <96652250+immisterio@users.noreply.github.com>
2022-12-07 15:30:26 +0200

    lifeevents

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -126,170 +126,191 @@ namespace Lampac.Controllers
         }
         #endregion
 
-        #region lite.js / events
+        #region lite.js
         [HttpGet]
         [Route("lite.js")]
+        async public Task<ActionResult> Lite()
+        {
+            if (!memoryCache.TryGetValue("ApiController:lite.js", out string file))
+            {
+                file = await IO.File.ReadAllTextAsync("plugins/lite.js");
+                memoryCache.Set("ApiController:lite.js", file, DateTime.Now.AddMinutes(5));
+            }
+
+            return Content(file.Replace("{localhost}", $"{AppInit.Host(HttpContext)}/lite"), contentType: "application/javascript; charset=utf-8");
+        }
+        #endregion
+
+        #region events
+        [HttpGet]
+        [Route("lifeevents")]
+        public ActionResult LifeEvents(int id)
+        {
+            string json = string.Empty;
+
+            if (memoryCache.TryGetValue($"ApiController:checkOnlineSearch:{id}", out (bool ready, int tasks, string online) res))
+            {
+                string online = res.online.Replace("{localhost}", $"{AppInit.Host(HttpContext)}/lite");
+                json = "{"+ $"\"ready\":{res.ready.ToString().ToLower()},\"tasks\":{res.tasks},\"online\":[{Regex.Replace(online, ",$", "")}]" + "}";
+            }
+
+            return Content(json, contentType: "application/javascript; charset=utf-8");
+        }
+
+        [HttpGet]
         [Route("lite/events")]
-        async public Task<ActionResult> Lite(int id, string imdb_id, long kinopoisk_id, string title, string original_title, string original_language, int year, string source, int serial = -1)
+        async public Task<ActionResult> Events(int id, string imdb_id, long kinopoisk_id, string title, string original_title, string original_language, int year, string source, int serial = -1, bool life = false)
         {
             string online = string.Empty;
             bool isanime = original_language == "ja";
 
-            if (HttpContext.Request.Path.Value.StartsWith("/lite/events") && AppInit.conf.jac.litejac)
-                online += "{name:'Jackett',url:'{localhost}/jac'},";
+            if (!life && AppInit.conf.jac.litejac)
+                online += "{\"name\":\"Jackett\",\"url\":\"{localhost}/jac\"},";
 
             if (!string.IsNullOrWhiteSpace(AppInit.conf.KinoPub.token))
-                online += "{name:'KinoPub',url:'{localhost}/kinopub'},";
+                online += "{\"name\":\"KinoPub\",\"url\":\"{localhost}/kinopub\"},";
 
             if (AppInit.conf.Filmix.enable)
-                online += "{name:'Filmix',url:'{localhost}/filmix'},";
+                online += "{\"name\":\"Filmix\",\"url\":\"{localhost}/filmix\"},";
 
             if (!string.IsNullOrWhiteSpace(AppInit.conf.Alloha.token))
-                online += "{name:'Alloha',url:'{localhost}/alloha'},";
+                online += "{\"name\":\"Alloha\",\"url\":\"{localhost}/alloha\"},";
 
             if (!string.IsNullOrWhiteSpace(AppInit.conf.Bazon.token))
-                online += "{name:'Bazon',url:'{localhost}/bazon'},";
+                online += "{\"name\":\"Bazon\",\"url\":\"{localhost}/bazon\"},";
 
             if (AppInit.conf.VideoDB.enable)
-                online += "{name:'VideoDB',url:'{localhost}/videodb'},";
+                online += "{\"name\":\"VideoDB\",\"url\":\"{localhost}/videodb\"},";
 
             if (AppInit.conf.Zetflix.enable)
-                online += "{name:'Zetflix',url:'{localhost}/zetflix'},";
+                online += "{\"name\":\"Zetflix\",\"url\":\"{localhost}/zetflix\"},";
 
             if (AppInit.conf.Kinobase.enable)
-                online += "{name:'Kinobase',url:'{localhost}/kinobase'},";
+                online += "{\"name\":\"Kinobase\",\"url\":\"{localhost}/kinobase\"},";
 
             if (AppInit.conf.Rezka.enable)
-                online += "{name:'HDRezka',url:'{localhost}/rezka'},";
+                online += "{\"name\":\"HDRezka\",\"url\":\"{localhost}/rezka\"},";
 
             if (AppInit.conf.VCDN.enable)
-                online += "{name:'VideoCDN',url:'{localhost}/vcdn'},";
+                online += "{\"name\":\"VideoCDN\",\"url\":\"{localhost}/vcdn\"},";
 
             if (AppInit.conf.Ashdi.enable)
-                online += "{name:'Ashdi (UKR)',url:'{localhost}/ashdi'},";
+                online += "{\"name\":\"Ashdi (UKR)\",\"url\":\"{localhost}/ashdi\"},";
 
             if (AppInit.conf.Eneyida.enable)
-                online += "{name:'Eneyida (UKR)',url:'{localhost}/eneyida'},";
+                online += "{\"name\":\"Eneyida (UKR)\",\"url\":\"{localhost}/eneyida\"},";
 
             if (!string.IsNullOrWhiteSpace(AppInit.conf.Kodik.token))
-                online += "{name:'Kodik',url:'{localhost}/kodik'},";
+                online += "{\"name\":\"Kodik\",\"url\":\"{localhost}/kodik\"},";
 
             if (!string.IsNullOrWhiteSpace(AppInit.conf.Seasonvar.token) && (serial == -1 || serial == 1))
-                online += "{name:'Seasonvar',url:'{localhost}/seasonvar'},";
+                online += "{\"name\":\"Seasonvar\",\"url\":\"{localhost}/seasonvar\"},";
 
             if (AppInit.conf.Lostfilmhd.enable && (serial == -1 || serial == 1))
-                online += "{name:'LostfilmHD',url:'{localhost}/lostfilmhd'},";
+                online += "{\"name\":\"LostfilmHD\",\"url\":\"{localhost}/lostfilmhd\"},";
 
             if (AppInit.conf.Collaps.enable)
-                online += "{name:'Collaps',url:'{localhost}/collaps'},";
+                online += "{\"name\":\"Collaps\",\"url\":\"{localhost}/collaps\"},";
 
             if (!string.IsNullOrWhiteSpace(AppInit.conf.HDVB.token))
-                online += "{name:'HDVB',url:'{localhost}/hdvb'},";
+                online += "{\"name\":\"HDVB\",\"url\":\"{localhost}/hdvb\"},";
 
             if (AppInit.conf.CDNmovies.enable && (serial == -1 || (serial == 1 && !isanime)))
-                online += "{name:'CDNmovies',url:'{localhost}/cdnmovies'},";
+                online += "{\"name\":\"CDNmovies\",\"url\":\"{localhost}/cdnmovies\"},";
 
             if (serial == -1 || isanime)
             {
                 if (AppInit.conf.AnilibriaOnline.enable)
-                    online += "{name:'Anilibria',url:'{localhost}/anilibria'},";
+                    online += "{\"name\":\"Anilibria\",\"url\":\"{localhost}/anilibria\"},";
 
                 if (AppInit.conf.Animevost.enable)
-                    online += "{name:'Animevost',url:'{localhost}/animevost'},";
+                    online += "{\"name\":\"Animevost\",\"url\":\"{localhost}/animevost\"},";
 
                 if (AppInit.conf.Animebesst.enable)
-                    online += "{name:'Animebesst',url:'{localhost}/animebesst'},";
+                    online += "{\"name\":\"Animebesst\",\"url\":\"{localhost}/animebesst\"},";
 
                 if (AppInit.conf.AnimeGo.enable)
-                    online += "{name:'AnimeGo',url:'{localhost}/animego'},";
+                    online += "{\"name\":\"AnimeGo\",\"url\":\"{localhost}/animego\"},";
 
                 if (AppInit.conf.AniMedia.enable)
-                    online += "{name:'AniMedia',url:'{localhost}/animedia'},";
+                    online += "{\"name\":\"AniMedia\",\"url\":\"{localhost}/animedia\"},";
             }
 
             if (AppInit.conf.Kinotochka.enable)
-                online += "{name:'Kinotochka',url:'{localhost}/kinotochka'},";
+                online += "{\"name\":\"Kinotochka\",\"url\":\"{localhost}/kinotochka\"},";
 
             if (serial == -1 || serial == 0 || (serial == 1 && !isanime))
             {
                 if (AppInit.conf.Kinokrad.enable)
-                    online += "{name:'Kinokrad',url:'{localhost}/kinokrad'},";
+                    online += "{\"name\":\"Kinokrad\",\"url\":\"{localhost}/kinokrad\"},";
 
                 if (AppInit.conf.Kinoprofi.enable)
-                    online += "{name:'Kinoprofi',url:'{localhost}/kinoprofi'},";
+                    online += "{\"name\":\"Kinoprofi\",\"url\":\"{localhost}/kinoprofi\"},";
 
                 if (AppInit.conf.Redheadsound.enable && (serial == -1 || serial == 0))
-                    online += "{name:'Redheadsound',url:'{localhost}/redheadsound'},";
+                    online += "{\"name\":\"Redheadsound\",\"url\":\"{localhost}/redheadsound\"},";
 
                 if (!string.IsNullOrWhiteSpace(AppInit.conf.VideoAPI.token) && (serial == -1 || serial == 0))
-                    online += "{name:'VideoAPI (ENG)',url:'{localhost}/videoapi'},";
+                    online += "{\"name\":\"VideoAPI (ENG)\",\"url\":\"{localhost}/videoapi\"},";
             }
 
             if (AppInit.conf.IframeVideo.enable && (serial == -1 || serial == 0))
-                online += "{name:'IframeVideo',url:'{localhost}/iframevideo'},";
+                online += "{\"name\":\"IframeVideo\",\"url\":\"{localhost}/iframevideo\"},";
 
             #region checkOnlineSearch
             if (AppInit.conf.online.checkOnlineSearch && id > 0)
             {
                 string memkey = $"ApiController:checkOnlineSearch:{id}";
-                if (!memoryCache.TryGetValue(memkey, out string newonline))
+                if (!memoryCache.TryGetValue(memkey, out (bool ready, int tasks, string online) cache))
                 {
+                    memoryCache.Set(memkey, string.Empty, DateTime.Now.AddSeconds(15));
+
                     var tasks = new List<Task>();
                     var links = new ConcurrentBag<(string code, int index, bool work)>();
 
-                    var match = Regex.Match(online, "(\\{name:'[^']+',url:'\\{localhost\\}/([^']+)'\\},)");
+                    var match = Regex.Match(online, "(\\{\"name\":\"[^\"]+\",\"url\":\"\\{localhost\\}/([^\"]+)\"\\},)");
                     while (match.Success)
                     {
                         if (!string.IsNullOrWhiteSpace(match.Groups[2].Value))
-                            tasks.Add(checkSearch(links, tasks.Count, match.Groups[1].Value, match.Groups[2].Value, id, imdb_id, kinopoisk_id, title, original_title, original_language, source, year, serial));
+                            tasks.Add(checkSearch(links, tasks, tasks.Count, match.Groups[1].Value, match.Groups[2].Value, id, imdb_id, kinopoisk_id, title, original_title, original_language, source, year, serial));
 
                         match = match.NextMatch();
                     }
 
-                    await Task.WhenAll(tasks);
-                    newonline = online = string.Join("", links.OrderByDescending(i => i.work).ThenBy(i => i.index).Select(i => i.code));
+                    if (life)
+                        return Content("{\"life\":true}", contentType: "application/javascript; charset=utf-8");
 
-                    memoryCache.Set(memkey, newonline, DateTime.Now.AddMinutes(10));
+                    await Task.WhenAll(tasks);
+
+                    cache.ready = true;
+                    cache.tasks = tasks.Count;
+                    cache.online = string.Join("", links.OrderByDescending(i => i.work).ThenBy(i => i.index).Select(i => i.code));
+
+                    memoryCache.Set(memkey, cache, DateTime.Now.AddMinutes(10));
                 }
 
-                online = newonline;
+                if (life)
+                    return Content("{\"life\":true}", contentType: "application/javascript; charset=utf-8");
+
+                online = cache.online;
             }
             #endregion
 
-            if (HttpContext.Request.Path.Value.StartsWith("/lite/events"))
-            {
-                string events = online.Replace("{localhost}", $"{AppInit.Host(HttpContext)}/lite");
-                events = events.Replace("'", "\"").Replace("{", "{\"").Replace(":\"", "\":\"").Replace("\",", "\",\"").Replace("\"show:", "\"show\":");
-
-                return Content($"[{Regex.Replace(events, ",$", "")}]", contentType: "application/javascript; charset=utf-8");
-            }
-
-            if (!memoryCache.TryGetValue($"ApiController:lite.js:{id > 0}", out string file))
-            {
-                if (id > 0)
-                    file = "var append = [{online}]";
-                else
-                    file = await System.IO.File.ReadAllTextAsync("plugins/lite.js");
-
-                memoryCache.Set($"ApiController:lite.js:{id > 0}", file, DateTime.Now.AddMinutes(5));
-            }
-
-            file = file.Replace("{online}", online);
-            file = file.Replace("{localhost}", $"{AppInit.Host(HttpContext)}/lite");
-
-            return Content(file, contentType: "application/javascript; charset=utf-8");
+            return Content($"[{Regex.Replace(online, ",$", "").Replace("{localhost}", $"{AppInit.Host(HttpContext)}/lite")}]", contentType: "application/javascript; charset=utf-8");
         }
         #endregion
 
 
         #region checkSearch
-        async Task checkSearch(ConcurrentBag<(string code, int index, bool work)> links, int index, string code, string uri,
+        async Task checkSearch(ConcurrentBag<(string code, int index, bool work)> links, List<Task> tasks, int index, string code, string uri,
                                int id, string imdb_id, long kinopoisk_id, string title, string original_title, string original_language, string source, int year, int serial)
         {
             string res = await HttpClient.Get($"{AppInit.Host(HttpContext)}/lite/{uri}?id={id}&imdb_id={imdb_id}&kinopoisk_id={kinopoisk_id}&title={HttpUtility.UrlEncode(title)}&original_title={HttpUtility.UrlEncode(original_title)}&original_language={original_language}&source={source}&year={year}&serial={serial}", timeoutSeconds: 10);
 
             bool work = !string.IsNullOrWhiteSpace(res) && res.Contains("data-json=");
-            links.Add((code.Replace("},", ",show:" + work.ToString().ToLower()+ "},"), index, work));
+            links.Add((code.Replace("},", $",\"index\":{index},\"show\":{work.ToString().ToLower()}" + "},"), index, work));
+
+            memoryCache.Set($"ApiController:checkOnlineSearch:{id}", (links.Count == tasks.Count, tasks.Count, string.Join("", links.OrderByDescending(i => i.work).ThenBy(i => i.index).Select(i => i.code))), DateTime.Now.AddMinutes(10));
         }
         #endregion
     }
