b1fe97e2ddf59f417b9808e0802910c300d08ae3
immisterio <96652250+immisterio@users.noreply.github.com>
2022-12-24 10:12:15 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,8 +4,10 @@ DEST="/home/lampac"
 systemctl stop lampac
 
 cd $DEST
+mv msx.json msx.json.back
 wget https://github.com/immisterio/Lampac/releases/latest/download/publish.zip
 unzip -o publish.zip
-rm -f publish.zip
+rm -f publish.zip msx.json
+mv msx.json.back msx.json
 
 systemctl start lampac
