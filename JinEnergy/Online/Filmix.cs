406607a228070efd6484d8aad16091e9714dbf0e
immisterio <96652250+immisterio@users.noreply.github.com>
2023-09-07 20:13:58 +0300

    filmix 4k

--- JinEnergy/Online/Filmix.cs
+++ JinEnergy/Online/Filmix.cs
@@ -8,7 +8,7 @@ namespace JinEnergy.Online
     public class FilmixController : BaseController
     {
         [JSInvokable("lite/filmix")]
-        async public static Task<string> Index(string args)
+        async public static ValueTask<string> Index(string args)
         {
             var arg = defaultArgs(args);
             int s = int.Parse(parse_arg("s", args) ?? "-1");
@@ -19,15 +19,13 @@ namespace JinEnergy.Online
             if (arg.original_language != "en")
                 clarification = 1;
 
-            string? hashfimix = null;
-            if (AppInit.Filmix.pro == false && string.IsNullOrEmpty(AppInit.Filmix.token))
-                hashfimix = await InvokeCache(0, "filmix:hash", () => JsHttpClient.Get("https://bwa.to/temp/hashfimix.txt", timeoutSeconds: 4));
+            string? hashfimix = await InvokeCache(0, "filmix:hash", gofreehash);
 
             var oninvk = new FilmixInvoke
             (
                null,
                AppInit.Filmix.corsHost(),
-               AppInit.Filmix.token,
+               hashfimix != null ? "bc170de3b2cafb09283b936011f054ed" : AppInit.Filmix.token,
                ongettourl => JsHttpClient.Get(AppInit.Filmix.corsHost(ongettourl)),
                onstreamtofile => replaceLink(hashfimix, onstreamtofile)
                //AppInit.log
@@ -50,21 +48,80 @@ namespace JinEnergy.Online
         }
 
 
+
+        static Random random = new Random();
+
+        async static ValueTask<string?> gofreehash()
+        {
+            if (AppInit.Filmix.pro != false || !string.IsNullOrEmpty(AppInit.Filmix.token))
+                return null;
+
+            string? hashfimix = null;
+
+            string? FXFS = await JsHttpClient.Get($"https://bwa.to/temp/hashfimix.txt?v={DateTime.Now.ToBinary()}", timeoutSeconds: 2);
+
+            if (!string.IsNullOrEmpty(FXFS))
+            {
+                string[] chars = new string[]
+                {
+                    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+                    "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m",
+                    "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"
+                };
+
+                for (int i = 0; i < 3; i++)
+                {
+                    string hash = chars[random.Next(0, chars.Length)] + chars[random.Next(0, chars.Length)] + chars[random.Next(0, chars.Length)];
+                    bool res = await checkHash(FXFS, hash);
+                    if (res)
+                    {
+                        hashfimix = $"{FXFS}{hash}";
+                        break;
+                    }
+                }
+            }
+
+            if (string.IsNullOrEmpty(hashfimix))
+                hashfimix = await JsHttpClient.Get($"https://bwa.to/temp/FXFS.txt?v={DateTime.Now.ToBinary()}", timeoutSeconds: 5);
+
+            if (!string.IsNullOrEmpty(hashfimix))
+                return hashfimix;
+
+            return null;
+        }
+
         static string replaceLink(string? hashfimix, string l)
         {
-            if (string.IsNullOrEmpty(hashfimix) || l.EndsWith("_480.mp4"))
+            if (string.IsNullOrEmpty(hashfimix))
                 return l;
 
-            var filmixservtime = DateTime.UtcNow.AddHours(2).Hour;
-            bool hidefree720 = string.IsNullOrWhiteSpace(AppInit.Filmix.token) && filmixservtime >= 19 && filmixservtime <= 23;
+            return Regex.Replace(l, "/s/[^/]+/", $"/s/{hashfimix}/");
+        }
 
-            if (!hidefree720 && l.EndsWith("_720.mp4"))
-                return l;
+        async static ValueTask<bool> checkHash(string FXFS, string hash)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.Timeout = TimeSpan.FromSeconds(2);
+                    client.MaxResponseContentBufferSize = 1_000_000; // 1MB
 
-            l = Regex.Replace(l, "/s/[^/]+/", hashfimix);
-            l = Regex.Replace(l, "^https?://", "");
+                    string url = $"https://nl107.cdnsqu.com/s/{FXFS}{hash}/UHD_90/In.deurim.2023.WEBRip.1080p_1080.mp4";
 
-            return "http://91.201.115.214:5823/" + l;
+                    using (HttpResponseMessage response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, url), HttpCompletionOption.ResponseHeadersRead))
+                    {
+                        if (((int)response.StatusCode) is 429 or 400)
+                            return false;
+
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
