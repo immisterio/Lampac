6664740b616995316df02dd17aa7fe812c21a560
immisterio <96652250+immisterio@users.noreply.github.com>
2023-11-22 17:50:32 +0200

    Update build.sh

--- Docker/build.sh
+++ Docker/build.sh
@@ -5,7 +5,8 @@ if test -f isdocker; then
 fi
 
 apt-get update
-apt-get install -y unzip ffmpeg 
+apt-get install -y unzip 
+#apt-get install -y ffmpeg 
 
 wget https://github.com/immisterio/Lampac/releases/latest/download/publish.zip
 unzip -o publish.zip && rm -f publish.zip && rm -rf ffprobe
