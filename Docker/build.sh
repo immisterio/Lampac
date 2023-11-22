90940ef71de199506201cf784799a6581ac3fb8a
immisterio <96652250+immisterio@users.noreply.github.com>
2023-11-22 17:43:17 +0200

    Create build.sh

---
+++ Docker/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if test -f isdocker; then
+  dotnet Lampac.dll
+fi
+
+apt-get update
+apt-get install -y unzip ffmpeg 
+
+wget https://github.com/immisterio/Lampac/releases/latest/download/publish.zip
+unzip -o publish.zip && rm -f publish.zip && rm -rf ffprobe
+
+touch isdocker
+
+dotnet Lampac.dll
