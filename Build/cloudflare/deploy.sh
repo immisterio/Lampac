44c8c6af93e7bc35c877568261d1b5f0fbb1bec8
immisterio <96652250+immisterio@users.noreply.github.com>
2023-12-25 14:48:35 +0200

    dev

--- Build/cloudflare/deploy.sh
+++ Build/cloudflare/deploy.sh
@@ -6,7 +6,8 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -InstallDir ./dotnet
 
-./dotnet/dotnet workload install wasm-tools
+#./dotnet/dotnet workload install wasm-tools
+./dotnet/dotnet workload install wasm-experimental
 ./dotnet/dotnet publish JinEnergy -c Release
 
 mkdir -p out/aot/
