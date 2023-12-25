3a365628bbe107d38fc5939ac9ca9487841b2ee1
immisterio <96652250+immisterio@users.noreply.github.com>
2023-12-25 18:10:51 +0200

    Update _middleware.ts

--- Build/cloudflare/functions/_middleware.ts
+++ Build/cloudflare/functions/_middleware.ts
@@ -1,15 +1,10 @@
-export async function onRequest(context) {
-  try {
-    var reqHeaders = new Headers(context.request.headers);
-    var response = await context.next();
+export const onRequest: PagesFunction = async (context) => {
+  var reqHeaders = new Headers(context.request.headers);
+  var response = await context.next();
   
-    response.headers.set('Access-Control-Allow-Origin', reqHeaders.get('Origin') || reqHeaders.get('Referer') || "*");
-    response.headers.set('Access-Control-Allow-Credentials', 'true');
-    response.headers.set('Access-Control-Allow-Methods', '*');
-    response.headers.set('Access-Control-Allow-Headers', '*');
-    return response;
-  } 
-  catch (err) {
-    return new Response(`${err.message}\n${err.stack}`, { status: 500 });
-  }
-}
+  response.headers.set('Access-Control-Allow-Origin', reqHeaders.get('Origin') || reqHeaders.get('Referer') || "*");
+  response.headers.set('Access-Control-Allow-Credentials', 'true');
+  response.headers.set('Access-Control-Allow-Methods', '*');
+  response.headers.set('Access-Control-Allow-Headers', '*');
+  return response;
+};
