399f3151588e7731fca8f737074effe24671bf6c
immisterio <96652250+immisterio@users.noreply.github.com>
2023-12-25 15:25:12 +0200

    dev

--- Build/cloudflare/deploy.sh
+++ Build/cloudflare/deploy.sh
@@ -7,9 +7,7 @@ chmod +x dotnet-install.sh
 ./dotnet-install.sh -InstallDir ./dotnet
 
 ./dotnet/dotnet workload install wasm-tools
-#./dotnet/dotnet workload install wasm-experimental
 ./dotnet/dotnet publish JinEnergy -c Release
-#./dotnet/dotnet build JinEnergy --configuration Release
 
 mkdir -p out/aot/
 cp -R Build/cloudflare/functions .
