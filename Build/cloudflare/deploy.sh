8493b038b9a0ee606a0206c6f7a83a170aa66f30
immisterio <96652250+immisterio@users.noreply.github.com>
2023-12-26 18:24:17 +0200

    cf

--- Build/cloudflare/deploy.sh
+++ Build/cloudflare/deploy.sh
@@ -1,10 +1,10 @@
-cat Build/cloudflare/JinEnergy.csproj > JinEnergy/JinEnergy.csproj
-cat Build/cloudflare/Shared.Engine.csproj > Shared.Engine/Shared.Engine.csproj
-cat Build/cloudflare/Shared.Model.csproj > Shared.Model/Shared.Model.csproj
+#cat Build/cloudflare/JinEnergy.csproj > JinEnergy/JinEnergy.csproj
+#cat Build/cloudflare/Shared.Engine.csproj > Shared.Engine/Shared.Engine.csproj
+#cat Build/cloudflare/Shared.Model.csproj > Shared.Model/Shared.Model.csproj
 
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh -InstallDir ./dotnet
+./dotnet-install.sh --channel 7.0.1xx -InstallDir ./dotnet
 ./dotnet/dotnet workload install wasm-tools
 ./dotnet/dotnet publish JinEnergy -c Release
 
