e1dc04148e3a5974f894e1fd474ab8f7693006e1
immisterio <96652250+immisterio@users.noreply.github.com>
2023-12-26 18:34:27 +0200

    fix

--- Build/cloudflare/deploy.sh
+++ Build/cloudflare/deploy.sh
@@ -1,6 +1,6 @@
 #cat Build/cloudflare/JinEnergy.csproj > JinEnergy/JinEnergy.csproj
-#cat Build/cloudflare/Shared.Engine.csproj > Shared.Engine/Shared.Engine.csproj
-#cat Build/cloudflare/Shared.Model.csproj > Shared.Model/Shared.Model.csproj
+cat Build/cloudflare/Shared.Engine.csproj > Shared.Engine/Shared.Engine.csproj
+cat Build/cloudflare/Shared.Model.csproj > Shared.Model/Shared.Model.csproj
 
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
