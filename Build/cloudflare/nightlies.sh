bbf2729da9940053445a00f0e8a9f8944cb9b183
immisterio <96652250+immisterio@users.noreply.github.com>
2024-01-12 12:39:17 +0200

    cf

--- Build/cloudflare/nightlies.sh
+++ Build/cloudflare/nightlies.sh
@@ -7,27 +7,28 @@ cp -R Lampac/bin/Release/net6.0/publish/* lpc/
 mkdir -p lpc/module
 
 ./dotnet6/dotnet publish DLNA -c Release
-cp Lampac/DLNA/bin/Release/net6.0/publish/DLNA.dll lpc/module/
+cp DLNA/bin/Release/net6.0/publish/DLNA.dll lpc/module/
 
 ./dotnet6/dotnet publish JacRed -c Release
-cp Lampac/JacRed/bin/Release/net6.0/publish/JacRed.dll lpc/module/
+cp JacRed/bin/Release/net6.0/publish/JacRed.dll lpc/module/
 
 ./dotnet6/dotnet publish Merchant -c Release
-cp Lampac/Merchant/bin/Release/net6.0/publish/Merchant.dll lpc/module/
+cp Merchant/bin/Release/net6.0/publish/Merchant.dll lpc/module/
 
 ./dotnet6/dotnet publish Online -c Release
-cp Lampac/Online/bin/Release/net6.0/publish/Online.dll lpc/module/
+cp Online/bin/Release/net6.0/publish/Online.dll lpc/module/
 
 ./dotnet6/dotnet publish SISI -c Release
-cp Lampac/SISI/bin/Release/net6.0/publish/SISI.dll lpc/module/
+cp SISI/bin/Release/net6.0/publish/SISI.dll lpc/module/
 
 ./dotnet6/dotnet publish TorrServer -c Release
-cp Lampac/TorrServer/bin/Release/net6.0/publish/TorrServer.dll lpc/module/
+cp TorrServer/bin/Release/net6.0/publish/TorrServer.dll lpc/module/
 
 ./dotnet6/dotnet publish Tracks -c Release
-cp Lampac/Tracks/bin/Release/net6.0/publish/Tracks.dll lpc/module/
+cp Tracks/bin/Release/net6.0/publish/Tracks.dll lpc/module/
 
 cd lpc/
 python -m zipfile -c update.zip *
 
 cd ../
+cat Build/cloudflare/nightlies_update.sh lpc/update.sh
---
+++ Build/cloudflare/nightlies_update.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+cd /home/lampac
+
+systemctl stop lampac
+
+rm -f update.zip
+wget https://bwa.pages.dev/lpc/update.zip
+unzip -o update.zip
+rm -f update.zip
+
+systemctl start lampac
