6cc1c504d7a2590c2d395a0c9dd63f84a6fd09c1
immisterio <96652250+immisterio@users.noreply.github.com>
2024-02-20 19:51:38 +0200

    fix docker

--- Build/Docker/build.sh
+++ Build/Docker/build.sh
@@ -6,6 +6,7 @@ fi
 
 apt-get update
 apt-get install -y unzip ffmpeg nano
+apt-get install -y libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev
 
 wget https://github.com/immisterio/Lampac/releases/latest/download/publish.zip
 unzip -o publish.zip && rm -f publish.zip
