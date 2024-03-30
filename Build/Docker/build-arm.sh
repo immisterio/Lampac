c8ae0566299df79966c89b325db46875a8e97b77
immisterio <96652250+immisterio@users.noreply.github.com>
2024-03-30 11:30:43 +0200

    Update build-arm.sh

--- Build/Docker/build-arm.sh
+++ Build/Docker/build-arm.sh
@@ -9,7 +9,7 @@ cd home
 
 apt-get update
 apt-get install -y wget unzip ffmpeg
-apt-get install -y chromium-browser
+apt-get install -y chromium
 #apt-get install -y libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev
 
 wget https://github.com/immisterio/Lampac/releases/latest/download/publish.zip
@@ -17,6 +17,6 @@ unzip -o publish.zip && rm -f publish.zip
 
 touch isdocker
 
-echo '{"puppeteer": {"executablePath": "/usr/bin/chromium-browser"}, "isarm": true, "mikrotik":true, "serverproxy": {"verifyip":false}}' > /home/init.conf
+echo '{"puppeteer": {"executablePath": "/usr/bin/chromium"}, "isarm": true, "mikrotik":true, "serverproxy": {"verifyip":false}}' > /home/init.conf
 
 dotnet Lampac.dll
