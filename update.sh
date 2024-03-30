47c24530db95d7310a791a79755de1f1db7945a5
immisterio <96652250+immisterio@users.noreply.github.com>
2024-03-30 19:56:15 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,18 +1,18 @@
-﻿#!/bin/bash
+#!/usr/bin/env bash
 DEST="/home/lampac"
 cd $DEST
 
 ver=$(cat vers.txt)
 gitver=$(curl -s https://api.github.com/repos/immisterio/Lampac/releases/latest | grep tag_name | sed s/[^0-9]//g)
-if [[ "$gitver" -gt "$ver" ]]
-    then
+if [ $gitver -gt $ver ]; then
     systemctl stop lampac
-
-    echo -n $gitver > vers.txt
+    echo "update lampac to version $gitver ..."
     rm -f update.zip
-    wget https://github.com/immisterio/Lampac/releases/latest/download/update.zip
+    curl -L -o update.zip https://github.com/immisterio/Lampac/releases/latest/download/update.zip
     unzip -o update.zip
     rm -f update.zip
-
+    echo -n $gitver > vers.txt
     systemctl start lampac
+else
+    echo "lampac already current version $ver"
 fi
