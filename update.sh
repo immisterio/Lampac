2639180bef4e09183e736ae60448a0d11ba741b4
immisterio <96652250+immisterio@users.noreply.github.com>
2024-10-10 12:58:13 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,10 +5,13 @@ cd $DEST
 ver=$(cat vers.txt)
 gitver=$(curl -k -s https://api.github.com/repos/immisterio/Lampac/releases/latest | grep tag_name | sed s/[^0-9]//g)
 if [ $gitver -gt $ver ]; then
-    systemctl stop lampac
-    echo "update lampac to version $gitver ..."
+    echo "update lampac to version $gitver"
     rm -f update.zip
-    curl -L -k -o update.zip https://github.com/immisterio/Lampac/releases/latest/download/update.zip
+    if ! curl -L -k -o update.zip https://github.com/immisterio/Lampac/releases/latest/download/update.zip; then
+        echo "Failed to download update.zip. Exiting."
+        exit 1
+    fi
+    systemctl stop lampac
     unzip -o update.zip
     rm -f update.zip
     echo -n $gitver > vers.txt
@@ -36,10 +39,13 @@ else
     dver=$(curl -k -s $BASE_URL/update/$ver.txt)
 	
     if [[ ${#dver} -eq 8 && $dver != $mver ]]; then
-        systemctl stop lampac
-        echo "update lampac to version $gitver ..."
+        echo "update lampac to version $gitver.$mver"
         rm -f update.zip
-        curl -L -k -o update.zip $BASE_URL/update/$dver.zip
+        if ! curl -L -k -o update.zip "$BASE_URL/update/$dver.zip"; then
+            echo "Failed to download update.zip. Exiting."
+            exit 1
+        fi
+        systemctl stop lampac
         unzip -o update.zip
         rm -f update.zip
         echo -n $dver > vers-minor.txt
