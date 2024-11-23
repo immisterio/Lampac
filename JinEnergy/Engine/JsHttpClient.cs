789b3e778267b43b1746e09e5c53624ed7d2ad8f
immisterio <96652250+immisterio@users.noreply.github.com>
2024-11-23 12:20:16 +0200

    fix

--- JinEnergy/Engine/JsHttpClient.cs
+++ JinEnergy/Engine/JsHttpClient.cs
@@ -19,7 +19,7 @@ namespace JinEnergy.Engine
                     hed.TryAdd(h.name, h.val);
             }
 
-            if (!hed.ContainsKey("User-Agent"))
+            if (useDefaultHeaders && !hed.ContainsKey("User-Agent"))
                 hed.TryAdd("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36");
 
             return hed;
