10f192ba88962387b55145b85b3861a4d407f5b8
immisterio <96652250+immisterio@users.noreply.github.com>
2024-12-11 10:35:31 +0200

    dev

--- Shared.Model/Online/Lumex/Episode.cs
+++ Shared.Model/Online/Lumex/Episode.cs
@@ -4,10 +4,6 @@
     {
         public int episode_id { get; set; }
 
-        public string name { get; set; }
-
-        public string poster { get; set; }
-
         public List<Medium> media { get; set; }
     }
 }
