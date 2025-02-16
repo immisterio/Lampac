5605468b5fd64cb16ba1465d62b696c15e2fdc78
immisterio <96652250+immisterio@users.noreply.github.com>
2025-02-16 18:55:35 +0200

    test

--- Build/cloudflare/deploy.sh
+++ Build/cloudflare/deploy.sh
@@ -1,7 +1,7 @@
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-#./dotnet-install.sh --channel 8.0.1xx -InstallDir ./dotnet
-./dotnet-install.sh --version 8.0.110 -InstallDir ./dotnet
+./dotnet-install.sh --channel 9.0.200 -InstallDir ./dotnet
+#./dotnet-install.sh --version 8.0.110 -InstallDir ./dotnet
 ./dotnet-install.sh --version 6.0.133 -InstallDir ./dotnet6
 
 chmod +x Build/cloudflare/nightlies.sh
@@ -12,9 +12,9 @@ chmod +x Build/cloudflare/nightlies.sh
 
 #cat Build/cloudflare/Shared.Engine.csproj > Shared.Engine/Shared.Engine.csproj
 #cat Build/cloudflare/Shared.Model.csproj > Shared.Model/Shared.Model.csproj
-cat Build/cloudflare/net8/JinEnergy.csproj > JinEnergy/JinEnergy.csproj
-cat Build/cloudflare/net8/Shared.Engine.csproj > Shared.Engine/Shared.Engine.csproj
-cat Build/cloudflare/net8/Shared.Model.csproj > Shared.Model/Shared.Model.csproj
+cat Build/cloudflare/net9/JinEnergy.csproj > JinEnergy/JinEnergy.csproj
+cat Build/cloudflare/net9/Shared.Engine.csproj > Shared.Engine/Shared.Engine.csproj
+cat Build/cloudflare/net9/Shared.Model.csproj > Shared.Model/Shared.Model.csproj
 
 ./dotnet/dotnet workload install wasm-tools
 ./dotnet/dotnet publish JinEnergy -c Release
@@ -22,7 +22,7 @@ cat Build/cloudflare/net8/Shared.Model.csproj > Shared.Model/Shared.Model.csproj
 mkdir -p out/
 
 cp -R Build/cloudflare/functions .
-cp -R JinEnergy/bin/Release/net8.0/publish/wwwroot/_framework/* out/
+cp -R JinEnergy/bin/Release/net9.0/publish/wwwroot/_framework/* out/
 
 if test -f "out/JinEnergy.wasm"; then
 	cat Build/cloudflare/_headers > out/_headers
