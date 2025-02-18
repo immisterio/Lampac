05d5d2fe37b89f254e4934e5415bd0e7d386f3cd
immisterio <96652250+immisterio@users.noreply.github.com>
2025-02-18 11:20:29 +0200

    Update nightlies_update.sh

--- Build/cloudflare/nightlies_update.sh
+++ Build/cloudflare/nightlies_update.sh
@@ -11,20 +11,20 @@ cd /home/lampac || { echo "Failed to change directory to /home/lampac. Exiting."
 
 rm -f update.zip
 
-echo "Download $UPDATEURI"
+echo -e "Download $UPDATEURI \n"
 
 if ! curl -L -k -o update.zip "$UPDATEURI"; then
-	echo "\n\nFailed to download update.zip. Exiting."
+	echo -e "\nFailed to download update.zip. Exiting."
 	exit 1
 fi
 if ! unzip -t update.zip; then
-	echo "\n\nFailed to test update.zip. Exiting."
+	echo -e "\nFailed to test update.zip. Exiting."
 	exit 1
 fi
 
-systemctl stop lampac || { echo "\n\nFailed to stop lampac service. Exiting."; exit 1; }
+systemctl stop lampac || { echo -e "\n\nFailed to stop lampac service. Exiting."; exit 1; }
 unzip -o update.zip
 rm -f update.zip
-systemctl start lampac || { echo "\n\nFailed to start lampac service. Exiting."; exit 1; }
+systemctl start lampac || { echo -e "\n\nFailed to start lampac service. Exiting."; exit 1; }
 
-echo "\n\nUpdate completed successfully."
+echo -e "\n\nUpdate completed successfully."
