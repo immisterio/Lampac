ec5fdc809421918f03576c94ad7f584c69665e4b
Ceniffin <ceniffin@gmail.com>
2025-07-08 15:38:30 +0800

    fix videohub

--- Online/Controllers/CDNvideohub.cs
+++ Online/Controllers/CDNvideohub.cs
@@ -1,10 +1,10 @@
-﻿using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Lampac.Engine.CORE;
 using Shared.Engine.CORE;
 using Online;
 using Shared.Model.Templates;
+using Newtonsoft.Json.Linq;
 
 namespace Lampac.Controllers.LITE
 {
@@ -12,7 +12,7 @@ namespace Lampac.Controllers.LITE
     {
         [HttpGet]
         [Route("lite/cdnvideohub")]
-        async public ValueTask<ActionResult> Index(string title, string original_title, long kinopoisk_id, bool origsource = false, bool rjson = false)
+        async public Task<ActionResult> Index(string title, string original_title, long kinopoisk_id, bool origsource = false, bool rjson = false)
         {
             var init = await loadKit(AppInit.conf.CDNvideohub);
             if (await IsBadInitialization(init, rch: true))
@@ -25,30 +25,59 @@ namespace Lampac.Controllers.LITE
             var proxyManager = new ProxyManager(init);
             var proxy = proxyManager.Get();
 
-            var cache = await InvokeCache<string>(rch.ipkey($"cdnvideohub:view:{kinopoisk_id}", proxyManager), cacheTime(20, rhub: 2, init: init), rch.enable ? null : proxyManager, async res =>
+            var cache = await InvokeCache<JObject>(rch.ipkey($"cdnvideohub:view:{kinopoisk_id}", proxyManager), cacheTime(20, rhub: 2, init: init), rch.enable ? null : proxyManager, async res =>
             {
                 if (rch.IsNotConnected())
                     return res.Fail(rch.connectionMsg);
 
-                string uri = $"{init.corsHost()}/svplayer?partner=27&kid={kinopoisk_id}";
-                string embed = rch.enable ? await rch.Get(uri, httpHeaders(init)) : await HttpClient.Get(uri, timeoutSeconds: 8, proxy: proxy, headers: httpHeaders(init));
-                if (embed == null)
-                    return res.Fail("embed");
+                string uri = $"{init.corsHost()}/api/v1/player/sv?pub=27&id={kinopoisk_id}&aggr=kp";
+                string json = rch.enable ? await rch.Get(uri, httpHeaders(init)) : await HttpClient.Get(uri, timeoutSeconds: 8, proxy: proxy, headers: httpHeaders(init));
+                if (string.IsNullOrEmpty(json))
+                    return res.Fail("json");
 
-                string file = Regex.Match(embed, "hlsUrl:([\t ]+)?'([^']+)'").Groups[2].Value;
-                if (string.IsNullOrEmpty(file))
-                    return res.Fail("file");
-
-                return file.Replace("u0026", "&").Replace("\\", "");
+                try
+                {
+                    var jsonData = JObject.Parse(json);
+                    return jsonData;
+                }
+                catch
+                {
+                    return res.Fail("parse json");
+                }
             });
 
             if (IsRhubFallback(cache, init))
                 goto reset;
 
-            return OnResult(cache, () => 
+            return OnResult(cache, () =>
             {
                 var mtpl = new MovieTpl(title, original_title);
-                mtpl.Append("По умолчанию", HostStreamProxy(init, cache.Value, proxy: proxy), vast: init.vast);
+
+                var jsonData = cache.Value;
+                var videos = jsonData["video"] as JArray;
+
+                if (videos != null && videos.Count > 0)
+                {
+                    foreach (var video in videos)
+                    {
+                        var sources = video["sources"];
+                        if (sources != null)
+                        {
+                            string hlsUrl = sources["hlsUrl"]?.ToString();
+                            string voiceStudio = video["voice_studio"]?.ToString() ?? "По умолчанию";
+                            string voiceType = video["voice_type"]?.ToString();
+
+                            if (!string.IsNullOrEmpty(hlsUrl))
+                            {
+                                string quality = voiceStudio;
+                                if (!string.IsNullOrEmpty(voiceType))
+                                    quality += $" ({voiceType})";
+
+                                mtpl.Append(quality, HostStreamProxy(init, hlsUrl.Replace("\\u0026", "&"), proxy: proxy), vast: init.vast);
+                            }
+                        }
+                    }
+                }
 
                 return rjson ? mtpl.ToJson() : mtpl.ToHtml();
 
