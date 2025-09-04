82585c018ae2a962bb246a0aec56aff916b284bb
immisterio <96652250+immisterio@users.noreply.github.com>
2025-09-04 15:24:04 +0300

    rch gzresult

--- Lampac/Controllers/RchApi.cs
+++ Lampac/Controllers/RchApi.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shared;
 using Shared.Engine;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Lampac.Controllers
 {
@@ -23,12 +27,53 @@ namespace Lampac.Controllers
         public ActionResult WriteResult([FromForm]string id, [FromForm]string value)
         {
             if (string.IsNullOrEmpty(id))
+            {
+                HttpContext.Response.StatusCode = 401;
                 return Content(string.Empty);
+            }
 
             if (!RchClient.rchIds.TryGetValue(id, out var tcs))
+            {
+                HttpContext.Response.StatusCode = 400;
                 return Content(string.Empty);
+            }
 
             tcs.SetResult(value ?? string.Empty);
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("rch/gzresult")]
+        async public Task<ActionResult> WriteZipResult([FromQuery]string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                HttpContext.Response.StatusCode = 401;
+                return Content(string.Empty);
+            }
+
+            if (!RchClient.rchIds.TryGetValue(id, out var tcs))
+            {
+                HttpContext.Response.StatusCode = 400;
+                return Content(string.Empty);
+            }
+
+            using (var compressedStream = new MemoryStream())
+            {
+                await Request.Body.CopyToAsync(compressedStream);
+                compressedStream.Position = 0;
+
+                using (var gzipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+                {
+                    using (var resultStream = new MemoryStream())
+                    {
+                        gzipStream.CopyTo(resultStream);
+                        string decompressedData = Encoding.UTF8.GetString(resultStream.ToArray());
+                        tcs.SetResult(decompressedData);
+                    }
+                }
+            }
+
             return Ok();
         }
     }
