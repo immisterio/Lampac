527750541778b6cdea4ecdaec864cc703e059364
ngcores <91776825+ngcores@users.noreply.github.com>
2026-05-07 19:34:51 +0300

    clear CacheVideo

--- Modules/CacheVideo/ModInit.cs
+++ Modules/CacheVideo/ModInit.cs
@@ -3,56 +3,55 @@ using Shared.Models.Module;
 using Shared.Models.Module.Interfaces;
 using System.Text.RegularExpressions;
 
-namespace CacheVideo
+namespace CacheVideo;
+
+public class ModInit : IModuleLoaded
 {
-    public class ModInit : IModuleLoaded
+    public void Loaded(InitspaceModel conf)
     {
-        public void Loaded(InitspaceModel conf)
+        EventListener.ProxyApiCacheStream += e =>
         {
-            EventListener.ProxyApiCacheStream += e =>
+            switch (e.decryptLink.plugin ?? "")
             {
-                switch (e.decryptLink.plugin ?? "")
-                {
-                    case "PornHub":
-                    case "Youjizz":
-                        {
-                            string uriKey = Regex.Replace(e.decryptLink.uri.Split("?")[0], "^https?://[^/]+", "");
-                            return ($"{e.decryptLink.plugin}:{uriKey}", "video/MP2T");
-                        }
-                    case "Xnxx":
-                    case "Xvideos":
-                        {
-                            string uriKey = Regex.Replace(e.decryptLink.uri.Split("?")[0], "^https?://[^/]+/[^,/]+", "");
-                            return ($"{e.decryptLink.plugin}:{uriKey}", "video/mp2t");
-                        }
-                    case "Xhamster":
-                        {
-                            string uriKey = Regex.Replace(e.decryptLink.uri.Split("?")[0], "^https?://[^/]+/[^/]+", "");
-                            return ($"{e.decryptLink.plugin}:{uriKey}", "video/MP2T");
-                        }
-                    default:
-                        return default;
-                }
-            };
+                case "PornHub":
+                case "Youjizz":
+                    {
+                        string uriKey = Regex.Replace(e.decryptLink.uri.Split("?")[0], "^https?://[^/]+", "");
+                        return ($"{e.decryptLink.plugin}:{uriKey}", "video/MP2T");
+                    }
+                case "Xnxx":
+                case "Xvideos":
+                    {
+                        string uriKey = Regex.Replace(e.decryptLink.uri.Split("?")[0], "^https?://[^/]+/[^,/]+", "");
+                        return ($"{e.decryptLink.plugin}:{uriKey}", "video/mp2t");
+                    }
+                case "Xhamster":
+                    {
+                        string uriKey = Regex.Replace(e.decryptLink.uri.Split("?")[0], "^https?://[^/]+/[^/]+", "");
+                        return ($"{e.decryptLink.plugin}:{uriKey}", "video/MP2T");
+                    }
+                default:
+                    return default;
+            }
+        };
 
-            EventListener.ProxyImgMd5key += e =>
-            {
-                switch (e.decryptLink.plugin ?? "")
-                {
-                    case "PornHub":
-                    case "Porntrex":
-                        {
-                            string uriKey = Regex.Replace(e.href.Split("?")[0], "^https?://[^/]+", "");
-                            return $"{e.decryptLink.plugin}:{uriKey}:{e.width}:{e.height}";
-                        }
-                    default:
-                        return default;
-                }
-            };
-        }
-
-        public void Dispose()
+        EventListener.ProxyImgMd5key += e =>
         {
-        }
+            switch (e.decryptLink.plugin ?? "")
+            {
+                case "PornHub":
+                case "Porntrex":
+                    {
+                        string uriKey = Regex.Replace(e.href.Split("?")[0], "^https?://[^/]+", "");
+                        return $"{e.decryptLink.plugin}:{uriKey}:{e.width}:{e.height}";
+                    }
+                default:
+                    return default;
+            }
+        };
+    }
+
+    public void Dispose()
+    {
     }
 }
