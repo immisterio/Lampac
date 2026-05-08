72a36125abdc4024df5f4d86582dfe28eacb9873
ngcores <91776825+ngcores@users.noreply.github.com>
2026-05-08 20:55:15 +0300

    Collaps Encoder uri

--- Modules/OnlineRUS/Collaps/ModInit.cs
+++ Modules/OnlineRUS/Collaps/ModInit.cs
@@ -1,18 +1,19 @@
 using Microsoft.AspNetCore.Http;
-using Shared.Models.Events;
+using Newtonsoft.Json.Linq;
+using Shared;
 using Shared.Models.Base;
+using Shared.Models.Events;
 using Shared.Models.Module;
 using Shared.Models.Module.Interfaces;
 using Shared.Services;
 using System.Collections.Generic;
-using Newtonsoft.Json.Linq;
-using Shared;
 
 namespace Collaps;
 
 public class ModInit : IModuleLoaded, IModuleOnline, IModuleOnlineSpider
 {
     public static ModuleConf conf;
+    public static string path;
 
     public List<ModuleOnlineItem> Invoke(HttpContext httpContext, RequestModel requestInfo, string host, OnlineEventsModel args)
     {
@@ -32,18 +33,22 @@ public class ModInit : IModuleLoaded, IModuleOnline, IModuleOnlineSpider
 
     public void Loaded(InitspaceModel baseconf)
     {
+        path = baseconf.path;
+
         CoreInit.conf.online.with_search.Add("collaps");
         CoreInit.conf.online.with_search.Add("collaps-dash");
 
         updateConf();
         EventListener.UpdateInitFile += updateConf;
         EventListener.OnlineApiQuality += onlineApiQuality;
+        EventListener.ProxyApiCreateHttpRequest += Encoder.ProxyApiCreateHttpRequest;
     }
 
     public void Dispose()
     {
         EventListener.UpdateInitFile -= updateConf;
         EventListener.OnlineApiQuality -= onlineApiQuality;
+        EventListener.ProxyApiCreateHttpRequest -= Encoder.ProxyApiCreateHttpRequest;
     }
 
     void updateConf()
---
+++ Modules/OnlineRUS/Collaps/Services/Encoder.cs
@@ -0,0 +1,51 @@
+﻿using Shared.Models.Events;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collaps;
+
+public static class Encoder
+{
+    const string marker = "/x-en-x/";
+
+    async public static Task ProxyApiCreateHttpRequest(EventProxyApiCreateHttpRequest e)
+    {
+        if (e.plugin != null && e.plugin.Equals("collaps", StringComparison.OrdinalIgnoreCase))
+        {
+            if (!e.requestMessage.RequestUri.AbsolutePath.Contains(marker))
+                e.requestMessage.RequestUri = new Uri(Uri(e.requestMessage.RequestUri.ToString(), true));
+        }
+    }
+
+    public static string Uri(string url, bool clearUri = false)
+    {
+        const string L = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        const string E = "DlChEXitLONYRkFjAsnBbymWzSHMqKPgQZpvwerofJTVdIuUcxaG";
+
+        if (string.IsNullOrWhiteSpace(url) || url.Contains(marker))
+            return url;
+
+        var uri = new Uri(url);
+
+        long n = (long)Math.Round(
+            DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() / 1000.0 / 60 / 60
+        );
+
+        string payload = $"{n}/{uri.AbsolutePath}{uri.Query}";
+
+        string base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(payload));
+
+        string encoded = new string(base64.Select(c =>
+        {
+            int index = L.IndexOf(c);
+            return index >= 0 ? E[index] : c;
+        }).ToArray());
+
+        if (clearUri)
+            return $"{uri.Scheme}://{uri.Authority}{marker}{encoded}";
+
+        return $"{uri.Scheme}://{uri.Authority}{marker}{encoded}" + (url.Contains(".mpd") ? "#.mpd" : "#.m3u8");
+    }
+}
