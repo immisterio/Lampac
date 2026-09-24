19ee0a056aeb4f13dad55573e64a03f7a0e624e2
Ilya Plotnikov <ilya.nc@mail.ru>
2026-09-24 10:54:50 +0300

    fix(scripts): upgradable_list check in install.sh (#234)

--- install.sh
+++ install.sh
@@ -602,15 +602,15 @@ manage_packages() {
   # Создаем временный файл для хранения списка upgradable-пакетов
   local upgradable_list=$(mktemp)
   # Получаем список пакетов, которые можно обновить.
-  # grep отсекает заголовок и предупреждения.
-  apt list --upgradable 2>/dev/null | grep -v "Listing..." > "$upgradable_list"
+  # sed отсекает заголовок и предупреждения.
+  apt list --upgradable 2>/dev/null | sed '/^Listing/d' > "$upgradable_list"
 
   # Проходим по каждому пакету из аргументов
   for pkg in "${packages[@]}"; do
     # Проверяем, установлен ли пакет
     if dpkg-query -W -f='${Status}' "$pkg" 2>/dev/null | grep -q "install ok installed"; then
       # Пакет установлен. Проверим, есть ли он в списке на обновление.
-      if grep -E "^$pkg/" "$upgradable_list" &>/dev/null; then
+      if [ -s "$upgradable_list" ] && grep -E "^$pkg/" "$upgradable_list" &>/dev/null; then
         packages_to_upgrade+=("$pkg")
       fi
     else
